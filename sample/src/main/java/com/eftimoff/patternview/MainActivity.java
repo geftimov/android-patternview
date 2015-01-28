ea57390fe1b0527d24cdb28a227f23d9cfbafda5
Georgi Eftimov <jokatavr@gmail.com>
2015-01-28 23:37:56 +0200

    Added the initial PatternView.

---
+++ sample/src/main/java/com/eftimoff/patternview/MainActivity.java
@@ -0,0 +1,18 @@
+package com.eftimoff.patternview;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private PatternView patternView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        patternView = (PatternView) findViewById(R.id.patternView);
+        patternView.setSize(6);
+    }
+}
